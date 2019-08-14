9248cf18a662be6b19ed044913c199dfce24254a
devapriyaa <bdevapriyaa@gmail.com>
2019-08-13 21:58:06 -0700

    added disabled ability to button

--- src/index.js
+++ src/index.js
@@ -7,10 +7,15 @@ class Game extends React.Component {
         super(props);
         this.state = {
             array: [1, 2, 3, 4, 5, 6, 7, 8],
-            clonedArray: [1, 2, 3, 4, 5, 6, 7, 8, 'X']
+            clonedArray: [1, 2, 3, 4, 5, 6, 7, 8, 'X'],
+            condition: false
         }
-
     }
+    handleClick() {
+        this.setState({
+          condition: !this.state.condition
+        })
+      }
     shuffle = (arr) => {
         for (var i = arr.length - 1; i > 0; i--) {
             var j = Math.floor(Math.random() * (i + 1));
@@ -39,19 +44,23 @@ class Game extends React.Component {
         if ((upperBox) >= 0 && this.state.array[upperBox] === "X") {
             console.log("upper")
             this.moved(upperBox, id, value);
+            this.handleClick();
         }
         if ((leftBox) >= 0 && this.state.array[leftBox] === "X") {
             console.log("left")
             this.moved(leftBox, id, value);
+            this.handleClick();
         }
         if (this.state.array[rightBox] === "X") {
             console.log("right")
             this.moved(rightBox, id, value);
+            this.handleClick();
         }
         if (this.state.array[lowerBox] === "X") {
 
             console.log("low")
             this.moved(lowerBox, id, value);
+            this.handleClick();
         }
     }
     moved = (targetBox, currentBox, value) => {
@@ -88,15 +97,15 @@ class Game extends React.Component {
             <h1>{this.props.name}</h1>
             <div className='box'>
 
-                <button id='0' value={this.state.array[0]} className='square' onClick={this.move}>{this.state.array[0]}</button>
-                <button id='1' value={this.state.array[1]} className='square' onClick={this.move}>{this.state.array[1]}</button>
-                <button id='2' value={this.state.array[2]} className='square' onClick={this.move}>{this.state.array[2]}</button>
-                <button id='3' value={this.state.array[3]} className='square' onClick={this.move}>{this.state.array[3]}</button>
-                <button id='4' value={this.state.array[4]} className='square' onClick={this.move}>{this.state.array[4]}</button>
-                <button id='5' value={this.state.array[5]} className='square' onClick={this.move}>{this.state.array[5]}</button>
-                <button id='6' value={this.state.array[6]} className='square' onClick={this.move}>{this.state.array[6]}</button>
-                <button id='7' value={this.state.array[7]} className='square' onClick={this.move}>{this.state.array[7]}</button>
-                <button id='8' value={this.state.array[8]} className='square' onClick={this.move}>{this.state.array[8]}</button>
+                <button id='0' value={this.state.array[0]} className='square' onClick={this.move} disabled={this.state.array[0]==="X"}>{this.state.array[0]}</button>
+                <button id='1' value={this.state.array[1]} className='square' onClick={this.move} disabled={this.state.array[1]==="X"}>{this.state.array[1]}</button>
+                <button id='2' value={this.state.array[2]} className='square' onClick={this.move} disabled={this.state.array[2]==="X"}>{this.state.array[2]}</button>
+                <button id='3' value={this.state.array[3]} className='square' onClick={this.move} disabled={this.state.array[3]==="X"}>{this.state.array[3]}</button>
+                <button id='4' value={this.state.array[4]} className='square' onClick={this.move} disabled={this.state.array[4]==="X"}>{this.state.array[4]}</button>
+                <button id='5' value={this.state.array[5]} className='square' onClick={this.move} disabled={this.state.array[5]==="X"}>{this.state.array[5]}</button>
+                <button id='6' value={this.state.array[6]} className='square' onClick={this.move} disabled={this.state.array[6]==="X"}>{this.state.array[6]}</button>
+                <button id='7' value={this.state.array[7]} className='square' onClick={this.move} disabled={this.state.array[7]==="X"}>{this.state.array[7]}</button>
+                <button id='8' value={this.state.array[8]} className='square' onClick={this.move} disabled={this.state.array[8]==="X"}>{this.state.array[8]}</button>
 
             </div>
         </div>
